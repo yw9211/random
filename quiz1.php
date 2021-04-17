cdeffcd0582e6c033887af5fd895a7f6535911de
yw9211 <78363653+yw9211@users.noreply.github.com>
2021-04-17 22:06:21 +0800

    Add files via upload

---
+++ quiz1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Quiz php</title>
+  </head>
+  <body>
+    <?php 
+    for ($i=0; $i<3; $i++)
+    echo '$i  ';
+     ?>
+  </body>
+</html>
+
+
+
